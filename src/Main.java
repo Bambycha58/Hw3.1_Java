162d4f66da8d10052a6ae4ed0bf71f8cfb07ad4e
Leonid Agureev <bambycha58@gmail.com>
2021-08-15 11:51:34 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 100;
+        int mile = 20;
+        int totalBonus = price / mile;
+        System.out.println("бонус мили " + totalBonus);
+
+
+    }
+}
